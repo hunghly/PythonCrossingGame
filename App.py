d6df9782cba7ba3bd17d2e19b565685b45d8d4bf
Hung Ly <hunghly@gmail.com>
2020-04-22 00:45:18 -0500

    learned some variables

---
+++ App.py
@@ -0,0 +1,31 @@
+# input_name = input('Enter a name: ')
+
+# output_text = 'Hello ' + input_name
+
+# print(output_text)
+
+print(1 + 1)
+
+name = 'hung'
+number = 1
+number2 = 2.2
+nothing = None
+condition = False
+
+if name: print('yes')
+
+print(type(name))
+print(type(number))
+print(type(number2))
+print(type(nothing))
+print(type(condition))
+condition_is_not = not condition # not operator is used to get the opposite condition
+say_hi = 'Hi ' + name
+print(condition_is_not)
+print(say_hi)
+
+num = 12
+floor_num = num // 10 # using // will floor the resulting number
+square_num = num ** 2 # use ** to square
+print(floor_num)
+print(square_num)
