d1b7f4c0a5ffd773d335c7c81070eaf89b6ec8bc
Hung Ly <hunghly@gmail.com>
2020-04-24 20:27:12 -0500

    worked on firebase python access

---
+++ Game.py
@@ -0,0 +1,27 @@
+import pygame
+
+print('hello')
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+WHITE_COLOR = (255, 255, 255)
+BLACK_COLOR = (0, 0, 0)
+clock = pygame.time.Clock()
+TICK_RATE = 60
+is_game_over = False
+
+# Create the window of specified size in width to display the game
+game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Set the game window color to white
+game_screen.fill(WHITE_COLOR)
+
+# while not is_game_over:
+
+    # for event in pygame.event.get():
+        # if event.ty
+
+    # pygame.display.update()
+    # clock.tick(TICK_RATE)
+
+
+
